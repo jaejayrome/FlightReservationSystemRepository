fd28c2cef03d06cc87fff680c88b2625586c3599
Jerome Goh <jerome.goh.jg@gmail.com>
2023-10-23 17:56:51 +0800

    added the partner entity

---
+++ FlightReservationSystem/FlightReservationSystemClassLibrary/src/entity/Partner.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jeromegoh
+ */
+@Entity
+public class Partner implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String companyName;
+    @Column(nullable = false)
+    private String loginUsername;
+    @Column(nullable = false)
+    private String loginPassword;
+
+    public Partner() {
+    }
+    
+    // constructor
+    public Partner(String companyName, String loginUsername, String loginPassword) {
+        this.companyName = companyName;
+        this.loginUsername = loginUsername;
+        this.loginPassword = loginPassword;
+    }
+    
+    // getters and setters
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getLoginUsername() {
+        return loginUsername;
+    }
+
+    public void setLoginUsername(String loginUsername) {
+        this.loginUsername = loginUsername;
+    }
+
+    public String getLoginPassword() {
+        return loginPassword;
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Partner)) {
+            return false;
+        }
+        Partner other = (Partner) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Partner[ id=" + id + " ]";
+    }
+    
+}
