4e12a56c6b794f80af7db55794d5acf7924f95c8
Jerome Goh <jerome.goh.jg@gmail.com>
2023-10-23 19:24:37 +0800

    initialised singleton and entity session beans

---
+++ FlightReservationSystem/FlightReservationSystemClassLibrary/src/util/exception/InitialDatabaseException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util.exception;
+
+/**
+ *
+ * @author jeromegoh
+ */
+public class InitialDatabaseException extends Exception{
+
+    public InitialDatabaseException() {
+    }
+
+    public InitialDatabaseException(String string) {
+        super(string);
+    }
+    
+}
